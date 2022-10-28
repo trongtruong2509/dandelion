7cc3957aba6951c05abaf32195e67fdfd2a48238
Trong Truong <trong.truong@seeingmachines.com>
2022-10-28 17:21:32 +0700

    add theme config raw 2

---
+++ src/themes/greenLight.js
@@ -0,0 +1,25 @@
+/*
+[data-theme=green-light] {
+    --layout-bg: #ced9d9;
+    --player-bg: #c0d8d8;
+    --sidebar-bg: hsla(0,0%,100%,0.3);
+    --sidebar-popup-bg: #cce0e0;
+    --alpha-bg: hsla(0,0%,100%,0.3);
+    --queue-player-popup-bg: #ced9d9;
+    --blur-queue-bg: rgba(206,217,217,0.9019607843137255);
+    --primary-bg: #e0ebeb;
+    --artist-layout-bg: rgba(206,217,217,0.8);
+    --purple-primary: #0e8080;
+    --text-item-hover: #0f7070;
+    --link-text-hover: #0f7070;
+    --chart-bg-img-alpha: rgb(206 217 217/0.9);
+}
+[data-theme=gray], [data-theme=green-light], [data-theme=pink-light] {
+    --box-item-bg: hsla(0,0%,100%,0.3);
+    --tab-active-bg: hsla(0,0%,100%,0.3);
+    --box-hot-item-bg: rgba(254,255,255,0.2);
+    --box-hot-item-bg-hover: rgba(254,255,255,0.3);
+    --text-secondary: #696969;
+}
+
+*/
---
+++ src/themes/light.js
@@ -0,0 +1,7 @@
+/*
+[data-theme=light] {
+    --thumbnail-player: rgba(0,0,0,0.2);
+    --box-hot-item-bg: rgba(20,20,20,0.05);
+    --box-hot-item-bg-hover: rgba(20,20,20,0.1);
+}
+*/
